0d31051ab1098b619b5342d4100e5d0f3756515a
jj-author@users.noreply.github.com <jj-author@users.noreply.github.com>
2024-10-20 22:46:00 +0200

    added PoC for client object manipulation for communication between CONNECT and inner/tunneled request

--- proxy-test/request_proxy.py
+++ proxy-test/request_proxy.py
@@ -11,14 +11,22 @@ class RequestPlugin(HttpProxyBasePlugin):
 
 
     def handle_client_request(self, request: HttpParser):
+        import random
+        # Check if the attribute exists, if not set it with a random number
+        if not hasattr(self.client, 'foo'):
+            self.client.foo = random.randint(1, 100)  # You can specify any range for the random number
+
         print(f'Request _url: {request._url}')
         print(f'Request.method: {request.method}')
         print(f'Request protocol: {request.protocol}')
         print(f'Request host: {request.host}')
         print(f'Request path: {request.path}')
+        print(f'Request protocol: {request.protocol}')
 
         print(f'Request properties: {vars(request)}')
-
+        print(f'##### Self.client: {self.client}')
+        print(f'##### Self.client vars: {vars(self.client)}')
+        print(f'##### Self vars: {vars(self)}')
         return request
 
 
@@ -28,10 +36,11 @@ if __name__ == '__main__':
         '--ca-key-file', 'ca-key.pem',
         '--ca-cert-file', 'ca-cert.pem',
         '--ca-signing-key-file', 'ca-signing-key.pem',
+#        '--enable-proxy-protocol'
     ]
     sys.argv += [
         '--hostname', '0.0.0.0',
-        '--port', '8899',
+        '--port', '8897',
         '--plugins', __name__ + '.RequestPlugin'
     ]
 
